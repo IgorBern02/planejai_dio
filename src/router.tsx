33b1aa78521008627fc244ab3c68d7b2bb2fc9d7
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2026-08-20 19:31:52 -0300

    feat: integrate Tailwind CSS and update project structure
    - Added Tailwind CSS and fontsource dependencies in package.json.
    - Removed App.css and migrated styles to Tailwind CSS and theme.css.
    - Updated App.tsx to use React Router for navigation.
    - Created a new router.tsx file for defining routes.
    - Removed unused assets (hero.png, react.svg, vite.svg).
    - Updated vite.config.ts to include Tailwind CSS plugin.
    - Refactored index.css to utilize Tailwind's utility classes.

---
+++ src/router.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    children: [
+      { path: "/", element: <h1>Home</h1> },
+      { path: "/resultado", element: <h1>Resultado</h1> },
+      { path: "/historico", element: <h1>Historico</h1> },
+    ],
+  },
+]);
