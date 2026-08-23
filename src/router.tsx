9f290de8672fe373b23486da50e7a5e5a70b6e11
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2026-08-23 13:10:50 -0300

    feat: add RootLayout component for routing structure

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
+import { RootLayout } from "./components/layout/RootLayout";
 
 export const router = createBrowserRouter([
   {
+    element: <RootLayout />,
     children: [
       { path: "/", element: <h1>Home</h1> },
       { path: "/resultado", element: <h1>Resultado</h1> },
